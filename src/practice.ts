68acb2dbec063bd8ba22beeae06eb51773f4cee2
Marius <marius@example.com>
2022-07-13 09:36:12 +0300

    practice

---
+++ src/practice.ts
@@ -0,0 +1,28 @@
+const usersT = [
+  {
+    id: 'u_1',
+    name: 'John',
+    town: 'London',
+    hasDog: false,
+  },
+  {
+    id: 'u_2',
+    name: 'Jill',
+    town: 'Kaunas',
+    hasDog: true,
+  },
+  {
+    id: 'u_3',
+    name: 'Serbentautas',
+    town: 'Klaipeda',
+    hasDog: true,
+  },
+];
+
+const rez = {
+  u_1: { name: 'John', town: 'London', hasDog: false },
+  u_2: { name: 'Jill', town: 'Kaunas', hasDog: true },
+  u_3: { name: 'Serbentautas', town: 'Klaipeda', hasDog: true },
+};
+
+// sukurti funkcija kuri perdaro is usersT ir rez
